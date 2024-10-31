76e4dd224f5003ffc056510c013b98d1a1a30d1b
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-31 22:00:10 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>Hava Durumu Uygulaması</h1>
+
+Geliştirdiğim hava durumu uygulamasında API aracılığıyla girilen şehrin anlık hava durumu bilgileri ekrana yansır.
+
+<h2> Sitemin ön yüz geliştirme aşamasında aşağıdaki teknolojiler kullanıldı: </h2>
+
+- HTML5
+- CSS
+- API
+
+<h2> Hava Durumu Sitemi Ziyaret Edin </h2>
+
+- https://my-weatherwebsite.netlify.app/
+
+<h4>Fotoğraf</h4>
+
+![](./1.png)
+
+![](./2.png)
---
+++ script.js
@@ -0,0 +1,40 @@
+const API_URL = "https://api.openweathermap.org/data/2.5/";
+const key = 'a24dcae6e6a821eb462b637a2cfbb296';
+
+const setQuery = (e) =>{
+    if(e.keyCode == "13"){
+        getResult(searchBar.value)
+    }
+}
+
+const getResult = (cityName) => {
+    let query = `${API_URL}weather?q=${cityName}&APPID=${key}&units=metric&lang=tr`;
+
+    fetch(query)
+        .then(weather => {
+            return weather.json()
+        })
+        .then(displayResult)
+
+    searchBar.value = "";
+}
+
+const displayResult = (weather) => {
+
+    console.log(weather)
+    let city = document.querySelector('.city')
+    city.innerText = `${weather.name}, ${weather?.sys?.country}`;
+
+    let temp = document.querySelector('.temp');
+    temp.innerText = `${Math.round(weather.main.temp)}°C`;
+
+    let desc = document.querySelector('.desc');
+    desc.innerText = weather?.weather[0]?.description;
+
+    let minmax = document.querySelector('.minmax');
+    minmax.innerText = `${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C`
+}
+
+const searchBar = document.getElementById("searchBar");
+searchBar.addEventListener('keypress', setQuery)
+
